2f1e5a4db3aab27e69a9be70e37c372e3402282f
Labinot <ferhatibryamov@abv.bg>
2026-08-25 22:04:14 +0300

    fix: improve iOS PWA safe area handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full dark ios-status-bar">
+      <body className="min-h-full dark ios-header">
         <ModalProvider>
           {children}
         </ModalProvider>
